d0261b9e46038ea289bbb1fbcd80cb045d25eae0
sourjo-ghosh <joyghosh211@outlook.com>
2026-03-09 07:32:39 +0600

    some bugs fixes and added some extra feat

--- script/index.js
+++ script/index.js
@@ -9,11 +9,31 @@ const loadLevelData = (id)=>{
     // console.log(url)
     fetch(url)
     .then(res => res.json())
-    .then(data => displayLevelWord(data.data))
+    .then(data => {
+        removeActive()
+        const clickedBtn = document.getElementById(`lesson-btn-${id}`)
+        // console.log(clickedBtn)
+        clickedBtn.classList.add("active")
+        displayLevelWord(data.data)
+    })
+}
+
+const loadWordDetails = async (id)=>{
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    console.log(url)
+    const res = await fetch(url);
+    const details = await res.json();
+    displayDetail(details.data)
+}
+const displayDetail = (word)=>{
+    console.log(word)
+}
+const removeActive = ()=>{
+    const lessonBtn = document.querySelectorAll('.lesson-btn')
+    lessonBtn.forEach(btn => btn.classList.remove("active"))
 }
 
 const displayLevelWord = (words)=>{
-    // console.log(words)
     const wordContainer= document.getElementById("word-container");
     wordContainer.innerHTML = "";
     if(words.length == 0 ){
@@ -26,17 +46,17 @@ const displayLevelWord = (words)=>{
     `
     }
     words.forEach((word) =>{
-    console.log(word)
     const card = document.createElement("div");
+    card.classList.add("h-full");
     card.innerHTML = `
-        <div class="bg-white rounded-xl shadow-sm text-center p-10">
+        <div class="bg-white rounded-xl shadow-sm text-center p-10 flex flex-col justify-between h-full min-h-[220px]">
             <div class="info space-y-3">
-                <h2 class="font-bold text-2xl">${word.word}</h2>
+                <h2 class="font-bold text-2xl">${word.word ? word.word:"Word Not Found"}</h2>
                 <p class="font-semibold ">Meaning/Pronunciation</p>
-                <p class="font-bangla font-medium text-2xl">${word.meaning}/${word.pronunciation}</p>
+                <p class="font-bangla font-medium text-2xl">${word.meaning ? word.meaning:"Word Not Found"}/${word.pronunciation ? word.pronunciation:"Word Not Found"}</p>
             </div>
             <div class="flex justify-between items-center mt-4">
-                <button class="btn bg-[#BADEFF]/50 hover:bg-[#BADEFF]">
+                <button onclick="loadWordDetails(${word.id})" class="btn bg-[#BADEFF]/50 hover:bg-[#BADEFF]">
                     <i class="fa-solid fa-circle-info"></i>
                 </button>
                 <button class="btn bg-[#BADEFF]/50 hover:bg-[#BADEFF]">
@@ -53,10 +73,9 @@ const displayLessons = (lessons)=>{
     const levelContainer = document.getElementById("level-container")
     levelContainer.innerHTML = "";
     for(let lesson of lessons){
-        // console.log(lesson)
         const lessonDiv = document.createElement("div")
         lessonDiv.innerHTML = `
-            <button onclick="loadLevelData(${lesson.level_no})" class="btn btn-outline btn-primary">
+            <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelData(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
             <i class="fa-solid fa-book-open"></i>  Lesson -${lesson.level_no}
             </button>
         `
@@ -64,7 +83,6 @@ const displayLessons = (lessons)=>{
     }
     
 }
-
-
-
 loadLessons()
+
+
